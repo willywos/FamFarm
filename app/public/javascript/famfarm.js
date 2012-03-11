13eb7d9ca92a8d896c24f9194e1c08cae9f43478
Willy <wwilimek@gmail.com>
2012-03-10 18:27:17 -0600

    udpating page layout and images

--- app/public/javascript/famfarm.js
+++ app/public/javascript/famfarm.js
@@ -1,8 +1,40 @@
 
+function load_images(startIndex) {
+	//alert('in here');
+	
+}
+
+
 $(document).ready(function() {
 	
-	//arc the logo on the page.
-	$("#logo").arctext({radius: 220, rotate: true});
-	$("#clouds").animate({  marginLeft: "+=650px", }, 1000 );
+	var index = 0;
+	load_images(index);
 	
+	$("#left_arrow").click(function() {
+		index = index - 1;
+		load_images(index)
+	});
+	
+	$("#right_arrow").click(function() {
+		index = index + 1;
+		load_images(index)
+	});
+	
+	$("#left_arrow").mouseover(function() {
+		$("#left_arrow").css("cursor", "pointer");
+		$("#left_arrow").css("background-image", "url('/images/left_arrow_over.png')");
+	});
+	
+	$("#left_arrow").mouseout(function() {
+		$("#left_arrow").css("background-image", "url('/images/left_arrow.png')");
+	});
+	
+	$("#right_arrow").mouseover(function() {
+		$("#right_arrow").css("cursor", "pointer");
+		$("#right_arrow").css("background-image", "url('/images/right_arrow_over.png')");
+	});
+	
+	$("#right_arrow").mouseout(function() {
+		$("#right_arrow").css("background-image", "url('/images/right_arrow.png')");
+	});
 });
